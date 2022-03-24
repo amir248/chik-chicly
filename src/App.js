4e399c10bbc9aeca4e51eba52aa2339c1d0013c6
chikchicly <chikchicly@gamil.com>
2022-03-24 18:20:52 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Header from './Header';
-// import Test from './test'
 
 class App extends Component {
   render() {
@@ -10,14 +8,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Прикольно да, да.</h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
       </div>
-      // <Header></Header>
     );
   }
 }
